692099339f0bae8b764de30d3965064770d6f242
Andres Ferraro <andres.ferraro@upf.edu>
2017-08-02 20:59:19 +0200

    Fix errors in create_mappings script

--- scripts/create_mappings.py
+++ scripts/create_mappings.py
@@ -1,5 +1,6 @@
 
 import psycopg2
+import re
 
 """
 Given a basic mapping file with this script we add all the missing mappings that take the information from the links tables
@@ -37,7 +38,7 @@ def main():
         for links in cur.fetchall():
             short_name = links[0].replace(' ', '_').replace('/', '_')
             reverse_short_name = re.sub('\{.*?\}','', links[2])
-            reverse_short_name = reverse_short_name.replace(' ', '_').replace('/', '_')
+            reverse_short_name = reverse_short_name.replace(' ', '_').replace('/', '_').replace("&","_")
             if co1+"2" == co2:
                 same_table_name = ".".join(t2.split('.')[0:-1])
                 same_table_field = t2.split('.')[-1]
@@ -57,7 +58,7 @@ def main():
                     d2rq:condition "simplebrainz.%s.link_type =%i " ;
                     d2rq:alias "%s as rg%d";
                     d2rq:join "%s=> simplebrainz.%s.%s" ;
-                    d2rq:join simplebrainz."%s.%s => rg%d.%s" .\n\n""" % (table, reverse_short_name, c2, reverse_short_name, c1, table, links[1], same_table_name, alias_counter, t1, table, co2, table, co1, alias_counter, same_table_field))
+                    d2rq:join "simplebrainz.%s.%s => rg%d.%s" .\n\n""" % (table, reverse_short_name, c2, reverse_short_name, c1, table, links[1], same_table_name, alias_counter, t1, table, co2, table, co1, alias_counter, same_table_field))
                 alias_counter +=1
 
             else:
@@ -68,7 +69,7 @@ def main():
                     d2rq:condition "simplebrainz.%s.link_type =%i " ;
                     d2rq:join "%s=> simplebrainz.%s.%s" ;
                     d2rq:join "simplebrainz.%s.%s => %s" .\n\n""" % (table, short_name, c1, short_name, c2, table, links[1], t1, table, co1, table, co2, t2))
-                output.append("""map:%s_%s a d2rq:PropertyBridge ;
+                output.append("""map:inverse_%s_%s a d2rq:PropertyBridge ;
                     d2rq:belongsToClassMap map:%s ;
                     d2rq:property sb:%s ;
                     d2rq:refersToClassMap map:%s ;
